c44611ce05b7137453680ee0a2e179669566c6ca
MichalRotkop <shalvi.r@gmail.com>
2024-05-22 21:25:30 +0300

    Adjust AddNote txt input and css

--- apps/note/cmps/AddNote.jsx
+++ apps/note/cmps/AddNote.jsx
@@ -1,14 +1,27 @@
+const { useState, useEffect } = React
+
 export function AddNote() {
+
+    const [isInputActive, setIsInputActive] = useState(false)
+
+    function showInputBox({ target }) {
+        if (target.value === '' || target.value.length > 1 || isInputActive === true) return
+        setIsInputActive(true)
+
+    }
+
     return <section className="add-note">
-        <h3>Take a note...</h3>
-        <div className="note-type">
+        <div className="input-container">
+            <input className="txt-input" autoFocus type="text"
+                placeholder="Take a note..." onInput={showInputBox} onClick={() => setIsInputActive(true)} />
+                
+
+        </div>
+        <div className="add-btns">
             <button><i className="fa-regular fa-square-check"></i></button>
             <button><i className="fa-regular fa-image"></i></button>
             <button><i className="fa-brands fa-youtube"></i></button>
-            {/* <i className="fa-regular fa-square-check"></i>
-            <i className="fa-regular fa-image"></i>
-            <i className="fa-brands fa-youtube"></i> */}
         </div>
     </section>
-
 }
+
