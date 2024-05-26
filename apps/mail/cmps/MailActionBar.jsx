0bd54653f64f64811dedce48a75eabc1560f98dc
ShaharGdn <shahargadon@gmail.com>
2024-05-26 19:08:57 +0300

    Set snooze state from action bar in mail details

--- apps/mail/cmps/MailActionBar.jsx
+++ apps/mail/cmps/MailActionBar.jsx
@@ -9,7 +9,8 @@ export function MailActionBar({ mail, onTrashClick, handleChange }) {
         {/* <input type="checkbox" name="" id="" onClick={onSelectAll} /> */}
         <span className="trash" onClick={onTrashClick}><i className="fa-light fa-trash-can"></i></span>
         {<span className="toggleRead" onClick={() => handleChange({ type: 'isRead', state: !isRead })}>{isRead ? <i className="fa-solid fa-envelope"></i> : <i className="fa-light fa-envelope-open"></i>}</span>}
-        <span className="snooze" onClick={() => handleChange({ type: 'isSnoozed', state: !isSnoozed })}><i className="fa-light fa-clock"></i></span>
+        {<span className="snooze" onClick={() => handleChange({ type: 'isSnoozed', state: !isSnoozed })}>{isSnoozed ? <i className="fa-sharp fa-solid fa-clock"></i> : <i className="fa-light fa-clock"></i>}</span>}
         <StarredMail isStarred={isStarred} handleChange={handleChange} />
     </section>
 }
+
