53a896d8db932a962a3a7ad832ca40f65ea6a056
ShaharGdn <shahargadon@gmail.com>
2024-06-06 15:46:41 +0300

    More home page styling fixed

--- views/Home.jsx
+++ views/Home.jsx
@@ -8,7 +8,7 @@ export function Home() {
             <NavLink to="/mail"><button className="hp-link"><img src="assets/imgs/gmail_link.png" /></button></NavLink>
             <NavLink to="/note"><button className="hp-link"><img src="assets/imgs/notes_link.png" /></button></NavLink>
         </section>
-        <img src="assets/imgs/2.png" alt="" />
+        <img className="team-img" src="assets/imgs/2.png" alt="" />
         <img className="footer-img" src="assets/imgs/3.png" alt="" />
         <AppHeader />
     </section>
